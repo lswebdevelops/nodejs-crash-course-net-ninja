c9171ae1e96e9bc9dbed6ea070b22547ae45102a
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-08 14:58:19 -0300

    creating a server using express and adding routes

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const express = require('express')
+const express = require("express");
 
-// express app 
+// express app
 
-
-const app  = express();
+const app = express();
 
 // listen for requests
-app.listen(3000)
+app.listen(3000);
 
-
-app.get('/', (req, res) => {
-    //setting the root path
- res.sendFile('./views/index.html', { root: __dirname})
-})
-app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
- res.sendFile('./views/about.html', { root: __dirname})
-})
+app.get("/", (req, res) => {
+  //setting the root path
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  // res.send('<p>about page</p>');
+  res.sendFile("./views/about.html", { root: __dirname });
+});
 // redirect
-app.get('/about-me', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.redirect('/about')
-})
-
+app.get("/about-me", (req, res) => {
+  // res.send('<p>about page</p>');
+  res.redirect("/about");
+});
 // 404 page
-
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname})
-})
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
