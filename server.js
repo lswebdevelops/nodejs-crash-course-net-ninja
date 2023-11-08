6bc1cbd65391fb7f28db385ec6be7a0e1ed8d937
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-08 13:01:12 -0300

    adding codeStates and redirections

--- server.js
+++ server.js
@@ -4,20 +4,40 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader("Content-Type", "text/html");
+
+  //path
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+    // page moved : 301      
+      res.statusCode = 301;
+      res.setHeader('Location', '/about')
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
   // send an html file
-  fs.readFile("./views/index.html", (err, data) => {
-    if(err) {
-        console.log(err);
-        res.end();
-    }
-    else{
-        // res.write(data)
-        // res.end();
-        // or
-        res.end(data);
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
     }
   });
-
 });
 
 server.listen(3000, "localhost", () => {
