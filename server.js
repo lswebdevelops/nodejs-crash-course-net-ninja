1cbff6c0c7bfb966566ee2a762a05795f35fc1c9
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-08 11:03:04 -0300

    adding an html file

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
-    // console.log(req.url, req.method);// "/"
-    // set header content type: 
-    // res.setHeader('Content-Type', 'text/plain')
-    // res.write("hello world!")
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<head><link rel="stylesheet" href="#" ></head>')
-    res.write("<h1>h1 part!</h1>")
-    res.write("<h2>h2 part!</h2>")
-    res.write("<p>hello world!</p>")
-
-    res.end();
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+  // send an html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if(err) {
+        console.log(err);
+        res.end();
+    }
+    else{
+        // res.write(data)
+        // res.end();
+        // or
+        res.end(data);
+    }
+  });
 
 });
 
-server.listen(3000 , 'localhost' , () => {
-    console.log('listening for requests on port 3000');
-})
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
+});
