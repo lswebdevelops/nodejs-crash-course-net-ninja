2423c8af04cdf7f52e359fdf663ae6e0a72960be
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-08 22:13:41 -0300

    create views using ejs

--- app.js
+++ app.js
@@ -4,23 +4,28 @@ const express = require("express");
 
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+// if a differente folder than view, as we are using view, it is not necessary
+// app.set('view' , 'myviews')
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
   //setting the root path
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render('index')
 });
 app.get("/about", (req, res) => {
   // res.send('<p>about page</p>');
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render('about')
+
 });
-// redirect
-app.get("/about-me", (req, res) => {
-  // res.send('<p>about page</p>');
-  res.redirect("/about");
-});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render('404')
 });
