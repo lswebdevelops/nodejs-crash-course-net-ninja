183a597cf15cb047788664214bd33db90ff54369
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-09 21:54:51 -0300

    printing a sible blog based on its id

--- app.js
+++ app.js
@@ -1,56 +1,94 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+
 // express app
 
 const app = express();
 
+//creating mongodb conection
+const dbURI =
+  "mongodb+srv://blog-ninja:wqrd25qMvhVuceT6@node-tuts.qo8q9kc.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  // only listen to requests, after connection is successiful
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
-app.set('view engine', 'ejs');
-// if a differente folder than view, as we are using view, it is not necessary
-// app.set('view' , 'myviews')
+app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
-// middleware
-// app.use((req, res, next) => {
-// console.log('new request made: ');
-// console.log('host: ', req.hostname);
-// console.log('path: ', req.path);
-// console.log('method: ', req.method);
-// next();// it hangs, if not added next()
-// });
-// // meddleware
-// app.use((req, res, next) => {
-// console.log('next middleware ');
+app.use(express.static("public")); // everything in the public forlder, will be avilable to frontend >> in the case the styles.css from head.ejs
+app.use(morgan("dev"));
 
-// next();// it hangs, if not added next()
-// });
+// mongoose and mongo sandbox routes:
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-// middleware & static files 
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.use(express.static('public'))// everything in the public forlder, will be avilable to frontend >> in the case the styles.css from head.ejs
-app.use(morgan('dev'));
+app.get('/single-blog', (req, res) => {
+  Blog.findById('654d3904124d318ea780db5e')
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+// routes
 
 app.get("/", (req, res) => {
-    //setting the root path
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How do defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ]
-//   res.render('index', { title: 'Home' , blogs:blogs}) or blogs
-  res.render('index', { title: 'Home' , blogs})
+  //setting the root path
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How do defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  //   res.render('index', { title: 'Home' , blogs:blogs}) or blogs
+  res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
   // res.send('<p>about page</p>');
-  res.render('about' , { title: 'About'})
-
+  res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create' , { title: 'New Blog'});
-})
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "New Blog" });
+});
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404' , { title: '404'})
+  res.status(404).render("404", { title: "404" });
 });
